efd7c73a5d9f3f5ea324eacf53afdec512996cb0
lvarciat <laurent.varciat@cohorte-technologies.com>
2023-04-12 14:45:16 +0200

    Correction  regex mail

--- src/components/fields/FieldMail.vue
+++ src/components/fields/FieldMail.vue
@@ -1,6 +1,6 @@
 <template>
     <input :id="id" :name="id" type="text" :size="size" :value="modelValue" :required="required"
-        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" data-format="john@smith.uk" />
+        pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$" data-format="john@smith.uk" />
 </template>
 
 <script setup>
