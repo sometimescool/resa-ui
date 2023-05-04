cc8b31c0f0a3cda9e147c663b3f9156c028b91d9
lvarciat <laurent.varciat@cohorte-technologies.com>
2023-05-04 08:40:21 +0200

    #18 - dialog

--- src/use/useMessageDialog.js
+++ src/use/useMessageDialog.js
@@ -1,5 +1,7 @@
 import { ref } from 'vue';
 
+let resolveClosed;
+
 const useMessageDialog = function () {
     const message = ref( {
         isVisible: false,
@@ -7,62 +9,19 @@ const useMessageDialog = function () {
         message: "",
         buttons: []
     } )
-    function showMessageConfirmCancel ( close, cancel ) {
-        show( 'Abandon des modifications.', { mess: { message: "Confimrmer l'abandon des modifications?" } }, [
-            {
-                label: 'Non',
-                action: close
-            }, {
-                label: 'Oui',
-                action: cancel,
-                default: true
-            }
-        ] )
-    }
-    function showMessageConfirmDelete ( close, confim ) {
-        show( "Suppression de l'élément courant", { mess: { message: "Confirmer la suppression ?" } }, [
-            {
-                label: 'Non',
-                action: close
-            }, {
-                label: 'Oui',
-                action: confim,
-                default: true
-            }
-        ] )
-    }
-    function showMessageDataChanged ( close, cancel, register ) {
-        show( 'Des modifications sont en cours.', { mess: { message: "Que voulez-vous faire des modifications ?" } }, [
-            {
-                label: 'Ne rien faire',
-                action: close
-            }, {
-                label: 'Les abandonner et continuer',
-                action: cancel
-            }, {
-                label: 'Les enregister et continuer',
-                action: register,
-                default: true
-            }
-        ] )
-    }
-    function showError ( action, errors ) {
-        show( "Erreur", errors, [
-            {
-                label: 'Fermer',
-                action: action,
-                default: true
-            }
-        ] )
-    }
-    function show ( title, messages, buttons ) {
+    async function show ( title, messages, buttons ) {
         setTitle( title )
         setContent( messages )
         setButtons( buttons )
-        message.value.isVisible = true
+        message.value.isVisible = true;
+        const response = await new Promise( function ( resolve ) {
+            resolveClosed = resolve
+        } );
+        return response
     }
-    function hide () {
-        message.value.isVisible = false
+    function setResponse ( action ) {
+        message.value.isVisible = false;
+        resolveClosed( action )
     }
     function setContent ( messages ) {
         let messageBody = ""
@@ -77,6 +36,6 @@ const useMessageDialog = function () {
     function setTitle ( title ) {
         message.value.title = title
     }
-    return [message, hide, showMessageDataChanged, showError, showMessageConfirmCancel, showMessageConfirmDelete]
+    return [message, show, setResponse]
 }
 export { useMessageDialog }
